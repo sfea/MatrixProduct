a3bbe49ce99fca3e3ca325907bb759719385d22d
Fedor <sfeaal@gmail.com>
2014-11-27 20:40:18 +0300

    Create first.c

---
+++ first.c
@@ -0,0 +1,52 @@
+                                                                                                      
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define FILE_1 "./file.txt"
+
+int main(int argc, char** argv)
+{
+        int n = atoi(argv[1]);
+        int fd = 0;
+        char* a;
+        int matr[2*n*n];
+        int i = 0, j = 0;
+        srand(time(NULL));
+
+        fd = open(FILE_1, O_CREAT|O_RDWR, 0666);
+        if (fd < 0)
+        {
+                perror("open");
+                exit(1);
+        }
+
+        for(i = 0; i < 2*n*n; i++)
+        {
+                matr[i] = rand() % 100;
+                if (i % n == 0 && i != 0)
+                        printf("\n");
+                if (i == n*n)
+                        printf("\n");
+                printf("%d ", matr[i]);
+        }
+
+        printf("\n");
+
+        if (write(fd, &n, sizeof(int)) != sizeof(int))
+        {
+                perror("write");
+                exit(1);
+        }
+
+        if (write(fd, matr, 2*n*n*sizeof(int)) != 2*n*n*sizeof(int))
+        {
+                perror("write");
+                exit(1);
+        }
+}
+
