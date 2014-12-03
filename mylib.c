13a3c32216fd6e0f2f0e1f031e050d096dc0551a
Fedor <sfeaal@gmail.com>
2014-12-03 20:57:03 +0300

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -3,41 +3,45 @@
 #include <string.h>
 #include <stdio.h>
 
-int new_read(char* buf, int old_fd)
+int* new_read(int old_fd, int* buf, int numb)
 {
-	int re = 1;
-	int i = 1;
-	for (i = 1;;i++)
-	{
-      if (re = read(old_fd, buf, 1024) == 1024)
-			buf = realloc(buf, i*1024);
-		else
-		{
-			buf = realloc(buf, (i - 1)*1024 + re);
-			break;
-		}
-		if (re < 0)
-			return -1;
-	}
+        int re = 1;
+        int i = 1;
+        int c = 0;
 
-	free(buf);
+        while(c != numb)
+        {
+                re = read(old_fd, buf + c, numb);
+                if(re < 0)
+                {
+                        perror("read");
+                        exit(-1);
+                }
+                c += re;
+        }
 }
 
-void new_write(char* buf, int new_fd)
+void new_write(int new_fd, int* buf, int numb)
 {
-	int c = 0;
-	int wr = 0;
-	int len = 0;
-	len = strlen(buf);
-	printf("%d\n", len);
-	while(wr = write(new_fd, buf + c, len - c) > 0)
-	{
-		c += wr;
-		if (len == c) break;
-		if (wr < 0)
-		{
-			perror("write");
-			break;
-		}
-	}
+        int c = 0, i = 0;
+        int wr;
+
+        for (i = 0;;i++)
+        {
+                wr = write(new_fd, buf + c, numb - c);
+                if (wr >= 0)
+                        c += wr;
+                if (numb == c)
+                {
+                        printf("Запись прошла успешно!\n");
+                        break;
+                }
+
+                if (wr < 0)
+                {
+                        perror("write");
+                        break;
+                }
+        }
 }
+
